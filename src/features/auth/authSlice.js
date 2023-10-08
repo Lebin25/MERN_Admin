3f4d6fd6c3d93c0a1b19ec1cdebbf484e5cf978b
lebin25 <lebin642@gmail.com>
2023-10-09 01:43:21 +0700

    Feature/Login Done

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,17 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import authService from './authService'
 
-const userDefaultState = {
-   _id: null,
-   firstname: null,
-   lastname: null,
-   email: null,
-   mobile: null,
-   token: null,
-}
+const getUserfromLocalStorage = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
 
 const initialState = {
-   user: userDefaultState,
+   user: getUserfromLocalStorage,
    isError: false,
    isLoading: false,
    isSuccess: false,
@@ -28,7 +21,7 @@ export const login = createAsyncThunk('auth/admin-login', async (user, thunkAPI)
 
 export const authSlice = createSlice({
    name: "auth",
-   initialState,
+   initialState: initialState,
    reducers: {},
    extraReducers: (builder) => {
       builder
@@ -39,12 +32,14 @@ export const authSlice = createSlice({
             state.isLoading = false;
             state.isSuccess = true;
             state.user = action.payload;
+            state.message = "Success";
          })
          .addCase(login.rejected, (state, action) => {
             state.isLoading = false;
             state.isError = true;
             state.isSuccess = false;
             state.user = null;
+            state.message = "Rejected";
          })
    }
 })
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomInput from '../components/CustomInput'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../features/auth/authSlice';
+
+let schema = Yup.object().shape({
+   email: Yup.string().email('Email is not valid').required('Email is required'),
+   password: Yup.string().required('Password is required'),
+});
 
 const Login = () => {
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   const formik = useFormik({
+      initialValues: {
+         email: '',
+         password: '',
+      },
+      validationSchema: schema,
+      onSubmit: (values) => {
+         dispatch(login(values))
+      },
+   });
+
+   const authState = useSelector((state) => state);
+   const { user, isLoading, isError, isSuccess, message } = authState.auth;
+
+   useEffect(() => {
+      if (isSuccess) {
+         navigate('admin')
+      }
+      else {
+         navigate('')
+      }
+   }, [user, isError, isSuccess, isLoading])
+
    return (
       <div className='py-5' style={{ background: "#ffd333", minHeight: "100vh" }}>
          <br />
@@ -13,13 +47,40 @@ const Login = () => {
          <div className='my-5 w-25 bg-white rounded-3 mx-auto p-4'>
             <h3 className='text-center title'>Login</h3>
             <p className='text-center'>Login to your account to coninue.</p>
-            <form action="">
-               <CustomInput type='text' label='Email Address' id='email' />
-               <CustomInput type='password' label='Password' id='pass' />
+            <div className="error text-center">
+               {message == "Rejected" ? "You are not an Admin" : ""}
+            </div>
+            <form action="" onSubmit={formik.handleSubmit}>
+               <CustomInput
+                  type='text'
+                  name='email'
+                  label='Email Address'
+                  id='email'
+                  val={formik.values.email}
+                  onCh={formik.handleChange('email')}
+               />
+               <div className="error">
+                  {formik.touched.email ? (
+                     <div>{formik.errors.email}</div>
+                  ) : null}
+               </div>
+               <CustomInput
+                  type='password'
+                  name='password'
+                  label='Password'
+                  id='pass'
+                  val={formik.values.password}
+                  onCh={formik.handleChange('password')}
+               />
+               <div className="error">
+                  {formik.touched.password ? (
+                     <div>{formik.errors.password}</div>
+                  ) : null}
+               </div>
                <div className='mb-3 text-end'>
                   <Link to='forgot-password' className=''>Forgot Password?</Link>
                </div>
-               <Link to='/admin' className='border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5' style={{ background: "#ffd333" }} type='submit'>Login</Link>
+               <button className='border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5' style={{ background: "#ffd333" }} type='submit'>Login</button>
             </form>
          </div>
       </div>
